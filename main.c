1e62047ccb54072066cca74a4085fcca47a248ac
gwa8520 <144990431+gwa8520@users.noreply.github.com>
2023-11-09 15:32:27 +0900

    23.11.09 3

--- main.c
+++ main.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
-	int i=0;
-	char str[4];
-	str[0] = 'a';
-	str[1] = 'b';
-	str[2] = 'c';
-	str[3] = '\0';
+	char src[]="The worst things to eat before you sleep.";
+	char dst[100];
 	
-	printf("%s\n",str);  //abc
+	//str copy
+	strcpy(dst, src);  //배열에 이름만 넣어 카피할 수 있다.  
 	
-	while(str[i]!='\0'){
-		printf("%c",str[i]);  //a b c 출력  
-		i++;
-	}
+	printf("copied string: %s\n", dst);
+
 	
 	return 0;
 }
